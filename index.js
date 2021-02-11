d9b5e0b5ff98162a5e9ce69f383ecf704906eb0f
nmhummel <natalie.hummel@gmail.com>
2021-02-10 20:19:40 -0600

    commit tree c6c41b985141d704524fa8ab9ceae5ac9034d613

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your functions here
+function map(sourceArray, num) {
+    let newArray = []
+  
+    for (let i = 0; i < sourceArray.length; i++) {
+      let x = sourceArray[i]
+      newArray.push(num(x))
+    }
+  
+    return newArray;
+}
+  
+function reduce(sourceArray, num, starting){
+    let newArray = (!!starting) ? starting : sourceArray[0]
+    let i = (!!starting) ? 0 : 1
+  
+    for (; i < sourceArray.length; i++) {
+        newArray = num(sourceArray[i], newArray)
+    }
+  
+    return newArray;
+}
